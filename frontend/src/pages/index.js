8f06165784fa4abd574a54f48bb7e28a1a05a7ed
kitsune-turing <tobongoomez809@gmail.com>
2026-02-04 21:20:57 -0500

    Add frontend/src/pages/index.js

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+export { LoginPage } from './LoginPage';
+export { RegisterPage } from './RegisterPage';
+export { DashboardPage } from './DashboardPage';
+export { CreateTaskPage } from './CreateTaskPage';
+export { EditTaskPage } from './EditTaskPage';
+export { ProfilePage } from './ProfilePage';
